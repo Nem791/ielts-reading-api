66ae8d19263ae4a287521b32866b0dbf11a0e408
Nem791 <phuongnam131201.nnnpn@gmail.com>
2021-12-07 21:17:00 +0700

    update

---
+++ test.js
@@ -0,0 +1,40 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const middlewares = jsonServer.defaults()
+
+let dbArray = ['listening.json', 'speaking.json', 'reading.json'];
+dbArray.forEach(element => {
+    // Set default middlewares (logger, static, cors and no-cache)
+    server.use(middlewares)
+
+    // Add custom routes before JSON Server router
+    server.get('/echo', (req, res) => {
+        res.jsonp(req.query)
+    })
+
+    // To handle POST, PUT and PATCH you need to use a body-parser
+    // You can use the one used by JSON Server
+    server.use(jsonServer.bodyParser)
+    server.use((req, res, next) => {
+        if (req.method === 'POST') {
+            req.body.createdAt = Date.now()
+        }
+        // Continue to JSON Server router
+        next()
+    })
+
+    const router = jsonServer.router(element);
+    console.log("router2: " + router);
+    // Use default router
+    server.use('/api', router);
+    //   next();
+})
+
+
+
+
+// Start sever
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log('JSON Server is running');
+});
